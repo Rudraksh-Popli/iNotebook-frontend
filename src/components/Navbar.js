a68a6efb664775eec1d7d8ca5bab9e405911dcd1
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-24 15:51:58 +0530

    Used useLocation Hook in React

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router';
 
 const NavBar = () => {
+    let location = useLocation();
+    useEffect(() => {
+        console.log(location.pathname);
+    }, [location])
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link to="/" end className="navbar-brand">Navbar</Link>
+                <Link to="/" end className="navbar-brand">iNotebook</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -13,10 +17,10 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link to="/" end className="nav-link active" aria-current="page">Home</Link>
+                            <Link to="/" end className={`nav-link${location.pathname==="/"?" active":""}`} aria-current="page">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/about" end  className="nav-link">About Us</Link>
+                            <Link to="/about" end className={`nav-link${location.pathname==="/about"?" active":""}`}>About Us</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
