bcd51fa3d4894e04ad9b558515a4723c6bdc97ce
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-24 19:31:06 +0530

    Added Functionality to Create a Note on Frontend

--- src/contexts/notes/NoteState.js
+++ src/contexts/notes/NoteState.js
@@ -68,8 +68,29 @@ const NoteState = (props) => {
         }
     ];
     const [notes, setNotes] = useState(notesIntitial);
+    // Add a Note
+    const addNote = (title, description, tag) => {
+        const note = {
+            "_id": "6a3bcd4e6b06096e35b4a876",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2026-06-24T12:23:43.383Z",
+            "__v": 0
+        };
+        setNotes(notes.concat(note));
+    };
+    // Delete a Note
+    const deleteNote = () => {
+
+    };
+    // Edit a Note
+    const editNote = () => {
+
+    };
     return (
-        <NoteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
             {props.children}
         </NoteContext.Provider>
     )
