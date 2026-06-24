86a2093d19941b1c269b30a63f923d322d35354e
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-24 18:00:53 +0530

    Displayed Notes in Card Format

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className='container my-3'>
             This is About
         </div>
     )
--- src/contexts/notes/NoteState.js
+++ src/contexts/notes/NoteState.js
@@ -11,6 +11,60 @@ const NoteState = (props) => {
             "tag": "Personal",
             "date": "2026-06-23T11:41:02.968Z",
             "__v": 0
+        },
+        {
+            "_id": "6a3bc73a2771e8eaf2ed7f84",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": "My Note4",
+            "description": "this is my fourth note yipeee",
+            "tag": "Personal",
+            "date": "2026-06-24T12:02:02.126Z",
+            "__v": 0
+        },
+        {
+            "_id": "6a3bc7422771e8eaf2ed7f85",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": "My Note5",
+            "description": "this is my fifth note yipeee",
+            "tag": "Personal",
+            "date": "2026-06-24T12:02:10.368Z",
+            "__v": 0
+        },
+        {
+            "_id": "6a3bcc316b06096e35b4a873",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": "My Note6",
+            "description": "this is my sixth note yipeee",
+            "tag": "Personal",
+            "date": "2026-06-24T12:23:13.283Z",
+            "__v": 0
+        },
+        {
+            "_id": "6a3bcc396b06096e35b4a874",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": "My Note7",
+            "description": "this is my seventh note yipeee",
+            "tag": "Personal",
+            "date": "2026-06-24T12:23:21.746Z",
+            "__v": 0
+        },
+        {
+            "_id": "6a3bcc456b06096e35b4a875",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": "My Note8",
+            "description": "this is my eighth note yipeee",
+            "tag": "Personal",
+            "date": "2026-06-24T12:23:33.904Z",
+            "__v": 0
+        },
+        {
+            "_id": "6a3bcc4e6b06096e35b4a876",
+            "user": "6a3a5f1c0ae3ad5110d98c48",
+            "title": "My Note9",
+            "description": "this is my ninth note yipeee",
+            "tag": "Personal",
+            "date": "2026-06-24T12:23:42.383Z",
+            "__v": 0
         }
     ];
     const [notes, setNotes] = useState(notesIntitial);
