6bf524f58155b90fcf5717925128241f74f15530
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-24 20:34:23 +0530

    Added Functionality to Delete a Note on Frontend

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import noteContext from '../contexts/notes/noteContext';
 
 const NoteItem = (props) => {
+    const context = useContext(noteContext);
+    const { deleteNote, editNote } = context;
     const { note } = props;
     return (
         <div className="col-md-3">
@@ -9,8 +12,12 @@ const NoteItem = (props) => {
                     <div className="d-flex justify-content-between align-items-center">
                         <h5 className="card-title mb-0">{note.title}</h5>
                         <div>
-                            <i className="i fa-solid fa-pen-to-square mx-2"></i>
-                            <i className="i fa-solid fa-trash-can mx-2"></i>
+                            <button className="btn btn-primary me-2" onClick={() => { editNote(note._id) }}>
+                                <i className="i fa-solid fa-pen-to-square"></i>
+                            </button>
+                            <button className="btn btn-danger ms-2" onClick={() => { deleteNote(note._id) }}>
+                                <i className="i fa-solid fa-trash-can" onClick={() => { deleteNote(note._id) }}></i>
+                            </button>
                         </div>
                     </div>
                     <p className="card-text mt-3">{note.description}</p>
--- src/contexts/notes/NoteState.js
+++ src/contexts/notes/NoteState.js
@@ -82,11 +82,15 @@ const NoteState = (props) => {
         setNotes(notes.concat(note));
     };
     // Delete a Note
-    const deleteNote = () => {
-
+    const deleteNote = (id) => {
+        console.log("deleting");
+        const newNotes = notes.filter((note) => {
+            return note._id !== id;
+        });
+        setNotes(newNotes);
     };
     // Edit a Note
-    const editNote = () => {
+    const editNote = (id, title, description, tag) => {
 
     };
     return (
