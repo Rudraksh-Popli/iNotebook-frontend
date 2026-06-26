0b48569aabf7d409fcb97255a5042fb37e5542a5
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-26 14:12:50 +0530

    Added First Draft of Signup Component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const Signup = () => {
+    let navigate = useNavigate();
+    const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" });
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json();
+        if (json.success) {
+            localStorage.setItem('token', json.authToken);
+            navigate("/");
+        }
+        else {
+            alert(json.error);
+        }
+    };
     return (
         <div className='container my-3'>
-            I am Signup
+            <form action="" method='post' onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" name="name" value={credentials.name} onChange={onChange} required minLength={3} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name="email" value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange} required minLength={8} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" id="cpassword" name="cpassword" value={credentials.cpassword} onChange={onChange} required minLength={8} />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 };
