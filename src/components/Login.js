7b535aa11fbdf9b81485638d526e75444bf2682a
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-26 14:28:34 +0530

    Added More Frontend Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,12 +30,12 @@ const Login = () => {
             <form action="" method='post' onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" value={credentials.email} required onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} id="password" name="password" />
+                    <input type="password" className="form-control" value={credentials.password} required minLength={8} onChange={onChange} id="password" name="password" />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
